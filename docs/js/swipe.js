fa78d206e08a0fe179d2cf7bdd10aa61e647b2b3
test-green <52778289+test-green@users.noreply.github.com>
2020-09-18 14:38:38 +0900

    change
    change

--- docs/js/swipe.js
+++ docs/js/swipe.js
@@ -1,4 +1,4 @@
-//1�ڂ�Swiper
+//1�ڂ�Swiper
 var mySwiper1 = new Swiper('.swiper1', {
 	loop: true,
 	speed: 1000,
@@ -19,7 +19,7 @@ var mySwiper1 = new Swiper('.swiper1', {
 	}
 });
 
-//2�ڂ�Swiper
+//2�ڂ�Swiper
 var mySwiper2 = new Swiper('.swiper2', {
 	loop: true,
 	speed: 1000,
@@ -35,6 +35,27 @@ var mySwiper2 = new Swiper('.swiper2', {
 	},
 	pagination: {
 		el: '.page2',
+		type: 'bullets',
+		clickable: true
+	}
+});
+
+//2�ڂ�Swiper
+var mySwiper3 = new Swiper('.swiper3', {
+	loop: true,
+	speed: 1000,
+	autoplay: {
+		delay: 3000,
+		stopOnLastSlide: false,
+		disableOnInteraction: false,
+		reverseDirection: false
+	},
+	navigation: {
+		nextEl: '.next3',
+		prevEl: '.prev3'
+	},
+	pagination: {
+		el: '.page3',
 		type: 'bullets',
 		clickable: true
 	}
